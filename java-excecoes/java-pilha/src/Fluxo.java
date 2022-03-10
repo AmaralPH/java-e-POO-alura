172e06a95bcd50f388c15d0aae565ac7e6281257
Pedro Amaral <amaralph1995@gmail.com>
2022-03-09 23:06:59 -0300

    feat: exercicios sobre classes de Exceptions [aula 4/6]

--- java-excecoes/java-pilha/src/Fluxo.java
+++ java-excecoes/java-pilha/src/Fluxo.java
@@ -5,28 +5,31 @@ public class Fluxo {
 		System.out.println("Ini do metodo MAIN");
 		try {
 			metodo1();
-		} catch (ArithmeticException | NullPointerException ex) {
-			 String msg = ex.getMessage();
-			 System.out.println("Exception: " + msg);
+		} catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
+			String msg = ex.getMessage();
+			System.out.println("Exception: " + msg);
 			ex.printStackTrace();
 		}
 		System.out.println("Fim do metodo MAIN");
 	}
 	
-	public static void metodo1() {
+//	public static void metodo1() {
+	public static void metodo1() throws MinhaExcecao {
 		System.out.println("Ini do metodo METODO 1");
 		metodo2();
 		System.out.println("Fim do metodo METODO 1");
 	}
 	
-	public static void metodo2() {
+//	public static void metodo2() {
+	public static void metodo2() throws MinhaExcecao {
 		System.out.println("Ini do metodo METODO 2");
 
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 			// int a = i / 0;
-			 Conta c = null;
-			 c.deposita();
+			// Conta c = null;
+			// c.deposita();
+			throw new MinhaExcecao("deu muito ruim");
 		}
 
 		System.out.println("Fim do metodo METODO 2");
---
+++ java-excecoes/java-pilha/src/MinhaExcecao.java
@@ -0,0 +1,8 @@
+
+// public class MinhaExcecao extends RuntimeException {
+public class MinhaExcecao extends Exception {
+
+	public MinhaExcecao(String msg) {
+		super(msg);
+	}
+}
