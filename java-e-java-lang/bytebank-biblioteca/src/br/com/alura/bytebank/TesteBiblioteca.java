24f79524793d0d74f7b97527bf84ee7d4725ad04
Pedro Amaral <amaralph1995@gmail.com>
2022-03-11 16:54:07 -0300

    feat: exercicios da aula sobre distribuição de código [3/5]

---
+++ java-e-java-lang/bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -0,0 +1,11 @@
+package br.com.alura.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+
+	public static void main(String[] args) {
+		Conta c = new ContaCorrente(123, 321);
+	}
+}
---
+++ java-e-java-lang/bytebank-herdado-conta/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"br.com.bytebank.banco.especial"},{"l":"br.com.bytebank.banco.modelo"},{"l":"br.com.bytebank.banco.teste"}]
