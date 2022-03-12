817db973f3e36ac310199086103d17fa3acb094b
Pedro Amaral <amaralph1995@gmail.com>
2022-03-12 19:14:48 -0300

    feat: guardando arrays de tipos diferentes [2/8]

--- java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,21 +1,27 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
     public static void main(String[] args) {
 
-        ContaCorrente[] contas = new ContaCorrente[5];
-
+        // alterando o tipo
+        Conta[] contas = new Conta[5];
         ContaCorrente cc1 = new ContaCorrente(22, 11);
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-
         contas[0] = cc1;
-        contas[1] = cc2;
 
-        System.out.println(contas[0].getNumero());
+        // cria instância de ContaPoupanca
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+        contas[1] = cc2;    
 
+        System.out.println(contas[1].getNumero()  );
+
+        // alterou o tipo, realizando o cast
+        ContaCorrente ref = (ContaCorrente) contas[0];
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
     }
-
 }
