e39d029db505bf3feef42fa4368a24e5eed77102
Pedro Amaral <amaralph1995@gmail.com>
2022-03-14 17:04:49 -0300

    feat: inicia o curso Java e java-io [1/8]

---
+++ java-io/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,32 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws IOException {
+
+		// FileInputStream é um InputStream
+		InputStream fis = new FileInputStream("lorem.txt");
+		// InputStreamReader é um Reader
+		Reader isr = new InputStreamReader(fis);
+		// BufferedReader não pode ser referenciado como Reader, 
+		// pois perderia o método readLine()
+		BufferedReader br = new BufferedReader(isr);
+		
+		String linha = br.readLine();
+		
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
+		br.close();
+	}
+
+}
